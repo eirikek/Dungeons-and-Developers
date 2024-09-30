76173b94d92955926fc9413b06a3b823ff10e400
christgh <christopher.hoee@gmail.com>
2024-09-30 17:01:02 +0200

    fix: tailwind error, add background color and add mock data

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,14 @@
-
-import './App.css'
+import './App.css';
+import MonsterCard from './components/MonsterCard/MonsterCard.tsx';
 
 function App() {
-
   return (
     <>
       <h1 className="text-6xl">Hello world!</h1>
-     </>
-  )
+      <MonsterCard />
+      {}
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/data/mockup.js
@@ -0,0 +1,476 @@
+// monsters
+
+export const monsters = {
+  count: 334,
+  results: [
+    {
+      index: 'aboleth',
+      name: 'Aboleth',
+      url: '/api/monsters/aboleth',
+    },
+    {
+      index: 'acolyte',
+      name: 'Acolyte',
+      url: '/api/monsters/acolyte',
+    },
+    {
+      index: 'adult-black-dragon',
+      name: 'Adult Black Dragon',
+      url: '/api/monsters/adult-black-dragon',
+    },
+    {
+      index: 'adult-blue-dragon',
+      name: 'Adult Blue Dragon',
+      url: '/api/monsters/adult-blue-dragon',
+    },
+    {
+      index: 'adult-brass-dragon',
+      name: 'Adult Brass Dragon',
+      url: '/api/monsters/adult-brass-dragon',
+    },
+    {
+      index: 'adult-bronze-dragon',
+      name: 'Adult Bronze Dragon',
+      url: '/api/monsters/adult-bronze-dragon',
+    },
+    {
+      index: 'adult-copper-dragon',
+      name: 'Adult Copper Dragon',
+      url: '/api/monsters/adult-copper-dragon',
+    },
+    {
+      index: 'adult-gold-dragon',
+      name: 'Adult Gold Dragon',
+      url: '/api/monsters/adult-gold-dragon',
+    },
+    {
+      index: 'adult-green-dragon',
+      name: 'Adult Green Dragon',
+      url: '/api/monsters/adult-green-dragon',
+    },
+    {
+      index: 'adult-red-dragon',
+      name: 'Adult Red Dragon',
+      url: '/api/monsters/adult-red-dragon',
+    },
+    {
+      index: 'adult-silver-dragon',
+      name: 'Adult Silver Dragon',
+      url: '/api/monsters/adult-silver-dragon',
+    },
+    {
+      index: 'adult-white-dragon',
+      name: 'Adult White Dragon',
+      url: '/api/monsters/adult-white-dragon',
+    },
+    {
+      index: 'air-elemental',
+      name: 'Air Elemental',
+      url: '/api/monsters/air-elemental',
+    },
+    {
+      index: 'ancient-black-dragon',
+      name: 'Ancient Black Dragon',
+      url: '/api/monsters/ancient-black-dragon',
+    },
+    {
+      index: 'ancient-blue-dragon',
+      name: 'Ancient Blue Dragon',
+      url: '/api/monsters/ancient-blue-dragon',
+    },
+    {
+      index: 'ancient-brass-dragon',
+      name: 'Ancient Brass Dragon',
+      url: '/api/monsters/ancient-brass-dragon',
+    },
+    {
+      index: 'ancient-bronze-dragon',
+      name: 'Ancient Bronze Dragon',
+      url: '/api/monsters/ancient-bronze-dragon',
+    },
+    {
+      index: 'ancient-copper-dragon',
+      name: 'Ancient Copper Dragon',
+      url: '/api/monsters/ancient-copper-dragon',
+    },
+    {
+      index: 'ancient-gold-dragon',
+      name: 'Ancient Gold Dragon',
+      url: '/api/monsters/ancient-gold-dragon',
+    },
+    {
+      index: 'ancient-green-dragon',
+      name: 'Ancient Green Dragon',
+      url: '/api/monsters/ancient-green-dragon',
+    },
+    {
+      index: 'ancient-red-dragon',
+      name: 'Ancient Red Dragon',
+      url: '/api/monsters/ancient-red-dragon',
+    },
+    {
+      index: 'ancient-silver-dragon',
+      name: 'Ancient Silver Dragon',
+      url: '/api/monsters/ancient-silver-dragon',
+    },
+    {
+      index: 'ancient-white-dragon',
+      name: 'Ancient White Dragon',
+      url: '/api/monsters/ancient-white-dragon',
+    },
+    {
+      index: 'androsphinx',
+      name: 'Androsphinx',
+      url: '/api/monsters/androsphinx',
+    },
+    {
+      index: 'animated-armor',
+      name: 'Animated Armor',
+      url: '/api/monsters/animated-armor',
+    },
+    {
+      index: 'ankheg',
+      name: 'Ankheg',
+      url: '/api/monsters/ankheg',
+    },
+    {
+      index: 'ape',
+      name: 'Ape',
+      url: '/api/monsters/ape',
+    },
+    {
+      index: 'archmage',
+      name: 'Archmage',
+      url: '/api/monsters/archmage',
+    },
+    {
+      index: 'assassin',
+      name: 'Assassin',
+      url: '/api/monsters/assassin',
+    },
+    {
+      index: 'awakened-shrub',
+      name: 'Awakened Shrub',
+      url: '/api/monsters/awakened-shrub',
+    },
+    {
+      index: 'awakened-tree',
+      name: 'Awakened Tree',
+      url: '/api/monsters/awakened-tree',
+    },
+    {
+      index: 'axe-beak',
+      name: 'Axe Beak',
+      url: '/api/monsters/axe-beak',
+    },
+    {
+      index: 'azer',
+      name: 'Azer',
+      url: '/api/monsters/azer',
+    },
+    {
+      index: 'baboon',
+      name: 'Baboon',
+      url: '/api/monsters/baboon',
+    },
+    {
+      index: 'badger',
+      name: 'Badger',
+      url: '/api/monsters/badger',
+    },
+    {
+      index: 'balor',
+      name: 'Balor',
+      url: '/api/monsters/balor',
+    },
+    {
+      index: 'bandit',
+      name: 'Bandit',
+      url: '/api/monsters/bandit',
+    },
+    {
+      index: 'bandit-captain',
+      name: 'Bandit Captain',
+      url: '/api/monsters/bandit-captain',
+    },
+    {
+      index: 'barbed-devil',
+      name: 'Barbed Devil',
+      url: '/api/monsters/barbed-devil',
+    },
+    {
+      index: 'basilisk',
+      name: 'Basilisk',
+      url: '/api/monsters/basilisk',
+    },
+    {
+      index: 'bat',
+      name: 'Bat',
+      url: '/api/monsters/bat',
+    },
+    {
+      index: 'bearded-devil',
+      name: 'Bearded Devil',
+      url: '/api/monsters/bearded-devil',
+    },
+    {
+      index: 'behir',
+      name: 'Behir',
+      url: '/api/monsters/behir',
+    },
+    {
+      index: 'berserker',
+      name: 'Berserker',
+      url: '/api/monsters/berserker',
+    },
+    {
+      index: 'black-bear',
+      name: 'Black Bear',
+      url: '/api/monsters/black-bear',
+    },
+    {
+      index: 'black-dragon-wyrmling',
+      name: 'Black Dragon Wyrmling',
+      url: '/api/monsters/black-dragon-wyrmling',
+    },
+    {
+      index: 'black-pudding',
+      name: 'Black Pudding',
+      url: '/api/monsters/black-pudding',
+    },
+    {
+      index: 'blink-dog',
+      name: 'Blink Dog',
+      url: '/api/monsters/blink-dog',
+    },
+    {
+      index: 'blood-hawk',
+      name: 'Blood Hawk',
+      url: '/api/monsters/blood-hawk',
+    },
+    {
+      index: 'blue-dragon-wyrmling',
+      name: 'Blue Dragon Wyrmling',
+      url: '/api/monsters/blue-dragon-wyrmling',
+    },
+    {
+      index: 'boar',
+      name: 'Boar',
+      url: '/api/monsters/boar',
+    },
+    {
+      index: 'bone-devil',
+      name: 'Bone Devil',
+      url: '/api/monsters/bone-devil',
+    },
+    {
+      index: 'brass-dragon-wyrmling',
+      name: 'Brass Dragon Wyrmling',
+      url: '/api/monsters/brass-dragon-wyrmling',
+    },
+    {
+      index: 'bronze-dragon-wyrmling',
+      name: 'Bronze Dragon Wyrmling',
+      url: '/api/monsters/bronze-dragon-wyrmling',
+    },
+    {
+      index: 'brown-bear',
+      name: 'Brown Bear',
+      url: '/api/monsters/brown-bear',
+    },
+    {
+      index: 'bugbear',
+      name: 'Bugbear',
+      url: '/api/monsters/bugbear',
+    },
+    {
+      index: 'bulette',
+      name: 'Bulette',
+      url: '/api/monsters/bulette',
+    },
+    {
+      index: 'camel',
+      name: 'Camel',
+      url: '/api/monsters/camel',
+    },
+    {
+      index: 'cat',
+      name: 'Cat',
+      url: '/api/monsters/cat',
+    },
+    {
+      index: 'centaur',
+      name: 'Centaur',
+      url: '/api/monsters/centaur',
+    },
+    {
+      index: 'chain-devil',
+      name: 'Chain Devil',
+      url: '/api/monsters/chain-devil',
+    },
+    {
+      index: 'chimera',
+      name: 'Chimera',
+      url: '/api/monsters/chimera',
+    },
+    {
+      index: 'chuul',
+      name: 'Chuul',
+      url: '/api/monsters/chuul',
+    },
+    {
+      index: 'clay-golem',
+      name: 'Clay Golem',
+      url: '/api/monsters/clay-golem',
+    },
+    {
+      index: 'cloaker',
+      name: 'Cloaker',
+      url: '/api/monsters/cloaker',
+    },
+    {
+      index: 'cloud-giant',
+      name: 'Cloud Giant',
+      url: '/api/monsters/cloud-giant',
+    },
+    {
+      index: 'cockatrice',
+      name: 'Cockatrice',
+      url: '/api/monsters/cockatrice',
+    },
+    {
+      index: 'commoner',
+      name: 'Commoner',
+      url: '/api/monsters/commoner',
+    },
+    {
+      index: 'constrictor-snake',
+      name: 'Constrictor Snake',
+      url: '/api/monsters/constrictor-snake',
+    },
+    {
+      index: 'copper-dragon-wyrmling',
+      name: 'Copper Dragon Wyrmling',
+      url: '/api/monsters/copper-dragon-wyrmling',
+    },
+    {
+      index: 'couatl',
+      name: 'Couatl',
+      url: '/api/monsters/couatl',
+    },
+    {
+      index: 'crab',
+      name: 'Crab',
+      url: '/api/monsters/crab',
+    },
+    {
+      index: 'crocodile',
+      name: 'Crocodile',
+      url: '/api/monsters/crocodile',
+    },
+    {
+      index: 'cult-fanatic',
+      name: 'Cult Fanatic',
+      url: '/api/monsters/cult-fanatic',
+    },
+    {
+      index: 'cultist',
+      name: 'Cultist',
+      url: '/api/monsters/cultist',
+    },
+    {
+      index: 'darkmantle',
+      name: 'Darkmantle',
+      url: '/api/monsters/darkmantle',
+    },
+  ],
+};
+
+// specific monster
+export const darkmantle = {
+  index: 'darkmantle',
+  name: 'Darkmantle',
+  size: 'Small',
+  type: 'monstrosity',
+  alignment: 'unaligned',
+  armor_class: [
+    {
+      type: 'dex',
+      value: 11,
+    },
+  ],
+  hit_points: 22,
+  hit_dice: '5d6',
+  hit_points_roll: '5d6+5',
+  speed: {
+    walk: '10 ft.',
+    fly: '30 ft.',
+  },
+  strength: 16,
+  dexterity: 12,
+  constitution: 13,
+  intelligence: 2,
+  wisdom: 10,
+  charisma: 5,
+  proficiencies: [
+    {
+      value: 3,
+      proficiency: {
+        index: 'skill-stealth',
+        name: 'Skill: Stealth',
+        url: '/api/proficiencies/skill-stealth',
+      },
+    },
+  ],
+  damage_vulnerabilities: [],
+  damage_resistances: [],
+  damage_immunities: [],
+  condition_immunities: [],
+  senses: {
+    blindsight: '60 ft.',
+    passive_perception: 10,
+  },
+  languages: '',
+  challenge_rating: 0.5,
+  proficiency_bonus: 2,
+  xp: 100,
+  special_abilities: [
+    {
+      name: 'Echolocation',
+      desc: "The darkmantle can't use its blindsight while deafened.",
+    },
+    {
+      name: 'False Appearance',
+      desc: 'While the darkmantle remains motionless, it is indistinguishable from a cave formation such as a stalactite or stalagmite.',
+    },
+  ],
+  actions: [
+    {
+      name: 'Crush',
+      desc: "Melee Weapon Attack: +5 to hit, reach 5 ft., one creature. Hit: 6 (1d6 + 3) bludgeoning damage, and the darkmantle attaches to the target. If the target is Medium or smaller and the darkmantle has advantage on the attack roll, it attaches by engulfing the target's head, and the target is also blinded and unable to breathe while the darkmantle is attached in this way.\nWhile attached to the target, the darkmantle can attack no other creature except the target but has advantage on its attack rolls. The darkmantle's speed also becomes 0, it can't benefit from any bonus to its speed, and it moves with the target.\nA creature can detach the darkmantle by making a successful DC 13 Strength check as an action. On its turn, the darkmantle can detach itself from the target by using 5 feet of movement.",
+      attack_bonus: 5,
+      damage: [
+        {
+          damage_type: {
+            index: 'bludgeoning',
+            name: 'Bludgeoning',
+            url: '/api/damage-types/bludgeoning',
+          },
+          damage_dice: '1d6+3',
+        },
+      ],
+      actions: [],
+    },
+    {
+      name: 'Darkness Aura',
+      desc: "A 15-foot radius of magical darkness extends out from the darkmantle, moves with it, and spreads around corners. The darkness lasts as long as the darkmantle maintains concentration, up to 10 minutes (as if concentrating on a spell). Darkvision can't penetrate this darkness, and no natural light can illuminate it. If any of the darkness overlaps with an area of light created by a spell of 2nd level or lower, the spell creating the light is dispelled.",
+      usage: {
+        type: 'per day',
+        times: 1,
+      },
+      actions: [],
+    },
+  ],
+  url: '/api/monsters/darkmantle',
+  legendary_actions: [],
+};
