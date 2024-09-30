798192134471c28760718138477e2e4d70d0f9fe
christgh <christopher.hoee@gmail.com>
2024-09-30 17:01:02 +0200

    fix: tailwind error, add background color and add mock data

--- frontend/src/hooks/useMonster.ts
+++ frontend/src/hooks/useMonster.ts
@@ -1,30 +1,34 @@
-import {useEffect, useState} from "react";
-
+import { useEffect, useState } from 'react';
 
 function useMonster(monsterName: string) {
-  const [data, setData] = useState<{ name: string; type: string; image: string; alignment: string; hit_points: number }>({
-    name: "",
-    type: "",
-    image: "",
-    alignment: "",
+  const [data, setData] = useState<{
+    name: string;
+    type: string;
+    image: string;
+    alignment: string;
+    hit_points: number;
+  }>({
+    name: '',
+    type: '',
+    image: '',
+    alignment: '',
     hit_points: 0,
   });
 
-
   useEffect(() => {
-    fetch("https://www.dnd5eapi.co/api/monsters/"+monsterName)
-      .then(response => response.json())
-      .then(json => setData(json))
-      .catch(error => console.log(error));
-  }, []);
+    fetch('https://www.dnd5eapi.co/api/monsters/' + monsterName)
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.log(error));
+  }, [monsterName]);
 
   return {
     name: data.name,
     type: data.type,
     img: data.image,
     alignment: data.alignment,
-    hp: data.hit_points
-  }
+    hp: data.hit_points,
+  };
 }
 
 export default useMonster;
