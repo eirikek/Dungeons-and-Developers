c5cc26dce60748a6768f9dc45a95346f43d04566
christgh <christopher.hoee@gmail.com>
2024-10-01 13:50:42 +0200

    feat: add icon for editing dungeon name and styled

---
+++ frontend/src/pages/App.tsx
@@ -0,0 +1,8 @@
+import '../App.css';
+// import Monsters from '../components/Monsters/Monsters.tsx';
+
+function App() {
+  return <>{/*<Monsters monsters={monsters} />*/}</>;
+}
+
+export default App;
