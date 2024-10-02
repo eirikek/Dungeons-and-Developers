1a8563d35fe790abc0cdd3cd7a60875cde9e73ce
christgh <christopher.hoee@gmail.com>
2024-10-02 09:46:16 +0200

    style: styles button

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,13 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      colors: {
+        customRed: '#D40101',
+      },
       backgroundImage: {
-        'terrain': 'url(\'/src/assets/terrain.jpeg\')',
+        terrain: "url('/src/assets/terrain.jpeg')",
       },
       keyframes: {
         underlineExpand: {
