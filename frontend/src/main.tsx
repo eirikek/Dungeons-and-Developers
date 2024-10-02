881c8810be4793d52c34cd8d400cd66266c6141f
christgh <christopher.hoee@gmail.com>
2024-10-02 13:31:07 +0200

    style: add background image

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './pages/App/App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// import App from './pages/App/App.tsx';
+import './index.css';
+import DungeonPage from './pages/DungeonPage.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <DungeonPage />
+  </StrictMode>
+);
--- frontend/src/pages/DungeonPage.tsx
+++ frontend/src/pages/DungeonPage.tsx
@@ -1,43 +1,39 @@
 import ModeEditOutlineIcon from '@mui/icons-material/ModeEditOutline';
 
 import Monsters from '../data/mockup.ts';
-import { useEffect, useState } from 'react';
 import '../components/MonsterCard/MonsterCard.css';
+import Navbar from '../components/Navbar/Navbar.tsx';
+import { useState } from 'react';
 
 export default function DungeonPage() {
   const name: string = 'The sunken citadel';
-  const [monsters, setMonsters] = useState(Monsters.results);
-
-  useEffect(() => {
-    setMonsters(monsters.slice(0, 6));
-  }, [monsters]);
+  const [monsters, setMonsters] = useState(Monsters.results.slice(0, 6));
   return (
     <>
-      <main className="flex flex-col text-center gap-20">
-        <header className="mt-8 flex flex-col gap-6 items-center">
-          {/*
-          Add image wiht object-cover:fit
-          */}
-          <h1 className="text-6xl">Dungeon creator</h1>
-          <span className="flex flex-row gap-4">
-            <h4 className="text-4xl">{name}</h4>
-            <button className="btn btn-reset hover:cursor-pointer rounded-lg hover:bg-customRed hover:text-white">
-              <ModeEditOutlineIcon fontSize="large" />
-            </button>
-          </span>
-        </header>
-        <section className="flex flex-col items-center">
-          {/*<h5 className="text-2xl w-1/2">Your dungeon monsters</h5>*/}
-          {/*
+      <Navbar />
+      <main className="relative flex flex-col text-center items-center mt-14 bg-storm_giant bg-cover h-screen before:absolute before:inset-0 before:bg-black before:opacity-70 before:z-0">
+        <section className="relative flex flex-col items-center gap-20 z-10">
+          <header className="mt-8 flex flex-col gap-10 items-center">
+            <h1 className="text-6xl text-white">Dungeon creator</h1>
+          </header>
+          <section className="flex flex-col items-center gap-12 bg-customGray">
+            <span className="flex flex-row gap-4">
+              <h4 className="text-4xl text-white">{name}</h4>
+              <button className="btn btn-reset hover:cursor-pointer rounded-lg hover:bg-customRed group">
+                <ModeEditOutlineIcon fontSize="large" className="text-white group-hover:text-black" />
+              </button>
+            </span>
+            {/*
           Add difficulty section -> check figma
           */}
-        </section>
-        <section className="grid grid-cols-3 items-center justify-center">
-          {monsters.map((monster, index) => (
-            <div key={index} className="card">
-              <p>{monster.name}</p>
-            </div>
-          ))}
+            <section className="grid grid-cols-3 items-center justify-center gap-8">
+              {monsters.map((monster, index) => (
+                <div key={index} className="card">
+                  <p>{monster.name}</p>
+                </div>
+              ))}
+            </section>
+          </section>
         </section>
       </main>
     </>
