04d15a8ba4851d07c35d8ebd3ba7f5fefccfd153
Mats <mats.kvanvik@hotmail.com>
2024-10-04 13:24:24 +0200

    feat #14: improved loading functionality
    Does not render the components twice anymore yay

--- frontend/src/components/MonsterCard/MonsterCard.tsx
+++ frontend/src/components/MonsterCard/MonsterCard.tsx
@@ -37,16 +37,16 @@ const MonsterCard: React.FC<MonsterCardProps> = ({ monsterName, onLoad }) => {
 
   return (
     <div className="flex flex-col items-center justify-center bg-black max-w-[20vw] rounded-[10px] shadow-[rgba(0,0,0,0.15)_1.95px_1.95px_2.6px]">
-      <h2 className="text-white font-bold text-lg">{monsterInfo.name}</h2>
       <img
         src={monsterImageURL}
         alt={monsterInfo.img ? 'Image of the monster' : 'No monster image found'}
-        className="max-w-[15vw] rounded-[15px] shadow-[0_2px_2px_0_rgba(0,0,0,1)]"
+        className="max-w-[15vw] rounded-[15px] shadow-[0_2px_2px_0_rgba(0,0,0,1) top-0 pt-0 mt-0]"
         onLoad={handleImageLoad}
         onError={handleImageError}
         style={{ display: imageLoaded ? 'block' : 'none' }}
       />
       {!imageLoaded && <div>Loading image...</div>}
+      <h2 className="text-white font-bold text-lg">{monsterInfo.name}</h2>
       <div className="mb-[3px] p-[5px] text-white font-semibold">
         <p>Type: {monsterInfo.type}</p>
         <p>HP: {monsterInfo.hp}</p>
--- frontend/src/pages/monsterPage/MonsterPage.tsx
+++ frontend/src/pages/monsterPage/MonsterPage.tsx
@@ -4,7 +4,7 @@ import mockup from '../../data/mockup.ts';
 import Navbar from '../../components/Navbar/Navbar.tsx';
 
 const monsterNameArray: string[] = mockup.results.map((result: any) => result.index);
-const monsterNameArray20: string[] = monsterNameArray.slice(0, 10);
+const monsterNameArray20: string[] = monsterNameArray.slice(0, 6);
 
 export default function MonsterPage() {
   const [loadedCount, setLoadedCount] = useState<number>(0);
@@ -21,27 +21,30 @@ export default function MonsterPage() {
   const isLoading = loadedCount < totalMonsters;
 
   return (
-    <div className="bg-madmage bg-center bg-cover bg-no-repeat h-screen w-screen">
+    <div className="bg-madmage bg-center bg-cover bg-no-repeat min-h-screen flex flex-col items-center justify-center">
       <Navbar />
-      {isLoading ? (
-        <div className="flex items-center justify-center h-full">
-          <p className="text-white text-xl font-bold">
-            Loading monsters... ({loadedCount} / {totalMonsters})
-          </p>
 
-          <div className="hidden">
-            {monsterNameArray20.map((monsterName) => (
-              <MonsterCard key={monsterName} monsterName={monsterName} onLoad={handleMonsterLoad} />
-            ))}
-          </div>
-        </div>
-      ) : (
-        <div className="bg-customGray bg-opacity-80 p-8 rounded-lg shadow-lg w-2/3 tablet:w-10/12 h-[80vh] w-[80vw] flex flex-wrap">
+      <div
+        className="flex flex-col items-center justify-center h-full"
+        style={{ display: isLoading ? 'block' : 'none' }}
+      >
+        <p className="text-white text-xl font-bold">
+          Loading monsters... ({loadedCount} / {totalMonsters})
+        </p>
+      </div>
+
+      <div
+        className="bg-customGray bg-opacity-80 p-8 rounded-lg shadow-lg w-2/3 tablet:w-10/12 h-auto flex flex-col items-center justify-center mt-10 mb-10"
+        style={{ display: isLoading ? 'none' : 'block' }}
+      >
+        <h2 className="text-2xl text-white font-bold mt-10">Monsters</h2>
+
+        <div className="grid grid-cols-3 gap-8 mt-8">
           {monsterNameArray20.map((monsterName) => (
             <MonsterCard key={monsterName} monsterName={monsterName} onLoad={handleMonsterLoad} />
           ))}
         </div>
-      )}
+      </div>
     </div>
   );
 }
