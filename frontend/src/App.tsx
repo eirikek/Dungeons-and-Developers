f8a725da293977ff0d35aa9db33e157f64f7d09e
eirikek <eirik.engen.kvam@gmail.com>
2024-10-05 20:36:46 +0200

    feat: layout of page finished with pagination

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/LoginPage/LoginPage.tsx';
 import DungeonPage from './pages/dungeonPage/DungeonPage.tsx';
+import EquipmentPage from './pages/equipmentPage/equipmentPage.tsx';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/project2" element={<LoginPage />} />
         <Route path="/project2/dungeon" element={<DungeonPage />} />
+        <Route path="/project2/equipment" element={<EquipmentPage />} />
       </Routes>
     </BrowserRouter>
   );
