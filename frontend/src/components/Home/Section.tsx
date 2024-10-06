0051e2ebabeea578d5ebe2a7054a903fff0783b8
eirikek <eirik.engen.kvam@gmail.com>
2024-10-06 23:08:52 +0200

    style: tweaked the animation duration

--- frontend/src/components/Home/Section.tsx
+++ frontend/src/components/Home/Section.tsx
@@ -11,14 +11,14 @@ interface SectionProps {
 export default function Section({ title, text, image, reversed = false }: SectionProps) {
   // Animation variants for text and header coming from the bottom
   const textVariants = {
-    hidden: { opacity: 0, y: 100 }, // Start from 50px below and invisible
-    visible: { opacity: 1, y: 0 }, // End at the original position and fully visible
+    hidden: { opacity: 0, y: 100 },
+    visible: { opacity: 1, y: 0 },
   };
 
   // Animation variants for the image coming from the left or right
   const imageVariants = {
-    hidden: { opacity: 0, x: reversed ? -150 : 150 }, // Start left if reversed, right otherwise
-    visible: { opacity: 1, x: 0 }, // End at the original position and fully visible
+    hidden: { opacity: 0, x: reversed ? -150 : 150 },
+    visible: { opacity: 1, x: 0 },
   };
 
   return (
@@ -31,8 +31,8 @@ export default function Section({ title, text, image, reversed = false }: Sectio
           className="w-full 2xl:w-3/5"
           initial="hidden"
           whileInView="visible"
-          viewport={{ once: true, amount: 0.3 }} // Trigger when 30% of section is in view
-          transition={{ duration: 1, ease: 'easeOut' }}
+          viewport={{ once: true, amount: 0.3 }}
+          transition={{ duration: 1.2, ease: 'easeOut' }}
           variants={textVariants}
         >
           <h2 className="sm:text-4xl text-3xl font-bold mb-4">{title}</h2>
@@ -45,7 +45,7 @@ export default function Section({ title, text, image, reversed = false }: Sectio
           initial="hidden"
           whileInView="visible"
           viewport={{ once: true, amount: 0.45 }}
-          transition={{ duration: 1, ease: 'easeOut' }}
+          transition={{ duration: 1.2, ease: 'easeOut' }}
           variants={imageVariants}
         >
           <img src={image} alt={title} className="shadow-none w-auto hidden 2xl:block" />
