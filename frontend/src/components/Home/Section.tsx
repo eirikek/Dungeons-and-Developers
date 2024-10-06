017130bff37cc1c457ad87eae54e482cf438c613
eirikek <eirik.engen.kvam@gmail.com>
2024-10-06 22:36:11 +0200

    feat: added scroll transitions on sections

--- frontend/src/components/Home/Section.tsx
+++ frontend/src/components/Home/Section.tsx
@@ -1,22 +1,55 @@
+import { motion } from 'framer-motion';
+import React from 'react';
+
 interface SectionProps {
   title: string;
-  text: JSX.Element;
+  text: React.ReactNode;
   image: string;
-  reversed?: boolean; // Hvis true, så vil layout være speilvendt
+  reversed?: boolean;
 }
 
 export default function Section({ title, text, image, reversed = false }: SectionProps) {
+  // Animation variants for text and header coming from the bottom
+  const textVariants = {
+    hidden: { opacity: 0, y: 100 }, // Start from 50px below and invisible
+    visible: { opacity: 1, y: 0 }, // End at the original position and fully visible
+  };
+
+  // Animation variants for the image coming from the left or right
+  const imageVariants = {
+    hidden: { opacity: 0, x: reversed ? -150 : 150 }, // Start left if reversed, right otherwise
+    visible: { opacity: 1, x: 0 }, // End at the original position and fully visible
+  };
+
   return (
     <section className="flex flex-col 3xl:w-3/4 w-5/6">
       <div
-        className={`flex gap-10 ${reversed ? '2xl:flex-row-reverse flex-col' : '2xl:flex-row flex-col'} laptop:flex-col items-center`}>
-        <div className="w-full 2xl:w-3/5">
+        className={`flex gap-10 ${reversed ? '2xl:flex-row-reverse flex-col' : '2xl:flex-row flex-col'} laptop:flex-col items-center`}
+      >
+        {/* Text and Header Fade-in Animation */}
+        <motion.div
+          className="w-full 2xl:w-3/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.3 }} // Trigger when 30% of section is in view
+          transition={{ duration: 1, ease: 'easeOut' }}
+          variants={textVariants}
+        >
           <h2 className="sm:text-4xl text-3xl font-bold mb-4">{title}</h2>
-          <p className="text-md lg:leading-10 sm:text-xl leading-8">{text}</p>
-        </div>
-        <div className="w-2/5">
+          <div className="text-md lg:leading-10 sm:text-xl leading-8">{text}</div>
+        </motion.div>
+
+        {/* Image Slide-in Animation */}
+        <motion.div
+          className="w-2/5"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.45 }}
+          transition={{ duration: 1, ease: 'easeOut' }}
+          variants={imageVariants}
+        >
           <img src={image} alt={title} className="shadow-none w-auto hidden 2xl:block" />
-        </div>
+        </motion.div>
       </div>
     </section>
   );
