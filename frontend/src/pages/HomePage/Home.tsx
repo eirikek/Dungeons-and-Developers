383ca7a01e96f36728059e6cfb94deb222e3a3db
eirikek <eirik.engen.kvam@gmail.com>
2024-10-06 22:56:11 +0200

    fix: changed text in sections

--- frontend/src/pages/HomePage/Home.tsx
+++ frontend/src/pages/HomePage/Home.tsx
@@ -4,6 +4,7 @@ import characterImg from '../../assets/characters.webp';
 import monstersImg from '../../assets/monsters.jpg';
 import hammerImg from '../../assets/hammer.png';
 import dragonGif from '../../assets/dragon.gif';
+import favoriteImg from '../../assets/favorite.png';
 import Tilt from 'react-parallax-tilt';
 import Section from '../../components/Home/Section.tsx';
 import { motion, useScroll, useTransform } from 'framer-motion';
@@ -88,7 +89,7 @@ export default function Home() {
                 </li>
                 <li>Equipment: Choose the weapons, armor, and gear your character will use during their adventure. From
                   swords and shields to magic wands and potions, your choice of equipment can significantly impact your
-                  character’s effectiveness in combat and exploration.
+                  character’s effectiveness in combat and exploration. You can choose up to 10 equipments
                 </li>
               </ul>
             </>
@@ -148,18 +149,17 @@ export default function Home() {
             <>
               <p className="mb-4">
                 In this section, you can create your own dungeon by selecting monsters to defend it. While exploring the
-                monster database, you can click the button next to any monster to add it to your custom dungeon.
+                monster database, you can click the
+                <span className="inline-block"><img src={favoriteImg} alt="favorite"
+                                                    className="shadow-none md:w-14 w-10 mx-2"></img></span>
+                button next to any monster to add it to your dungeon. If you change your mind, you can also easily
+                remove monsters from your dungeon to adjust your selection.
               </p>
               <p className="mb-4">
-                As the Dungeon Master, you are responsible for selecting up to 6 monsters to populate your dungeon.
+                You can select up to 6 monsters to populate your dungeon.
                 Choose your monsters wisely—whether you want a balanced combination of brute strength and magic, or a
                 lineup of agile, stealthy creatures, the choice is yours. Consider each monster’s abilities, strengths,
                 and weaknesses to create a challenging experience for those who dare to explore your dungeon.
-              </p>
-              <p>
-                Once your dungeon is complete, you can share it with others or test it out yourself to see how well your
-                selection of monsters performs in defending your treasure. Let your creativity run wild as you craft the
-                ultimate dungeon experience!
               </p>
             </>
           }
