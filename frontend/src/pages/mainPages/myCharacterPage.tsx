75f09f06d8281efcb607ac244259a4adb1627a88
eirikek <eirik.engen.kvam@gmail.com>
2024-10-10 11:16:16 +0200

    feat: Get ability data from api on mycharacter page

--- frontend/src/pages/mainPages/myCharacterPage.tsx
+++ frontend/src/pages/mainPages/myCharacterPage.tsx
@@ -1,5 +1,4 @@
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
-import Button from '../../components/CustomButton/CustomButton.tsx';
 import Counter from '../../components/Counter/Counter.tsx';
 import React, { useEffect, useState } from 'react';
 import MainPageLayout from '../../components/Layouts/MainPageLayout.tsx';
@@ -7,6 +6,7 @@ import raceImageMapping from '../../utils/raceImageMapping.ts';
 import classImageMapping from '../../utils/classImageMapping.ts';
 import useRaces from '../../hooks/useRaces.ts';
 import useClasses from '../../hooks/useClasses.ts';
+import useAbilityScores from '../../hooks/useAbilityScores.ts';
 
 interface Race {
   name: string;
@@ -25,16 +25,18 @@ interface Class {
 
 const MyCharacterPage: React.FC<MyCharacterProps> = ({ raceChange, classChange }) => {
 
-  const [abilities, setAbilities] = useState([0, 0, 0, 0, 0, 0]);
+  const abilities = ['cha', 'con', 'dex', 'int', 'str', 'wis'];
+  const abilityData = abilities.map(useAbilityScores);
+  const [abilityValue, setAbilityValue] = useState([0, 0, 0, 0, 0, 0]);
 
   // Race state and handler
   const [races, setRaces] = useState<Race[]>([]);
   const [raceIndex, setRaceIndex] = useState(0);
 
   const handleAbilityChange = (index: number, newValue: number) => {
-    const updatedAbilities = [...abilities];
+    const updatedAbilities = [...abilityValue];
     updatedAbilities[index] = newValue;
-    setAbilities(updatedAbilities);
+    setAbilityValue(updatedAbilities);
   };
 
   const handleNextPageRace = () => {
@@ -157,29 +159,31 @@ const MyCharacterPage: React.FC<MyCharacterProps> = ({ raceChange, classChange }
     classes,
   ]);
 
+
   return (
     <MainPageLayout>
       <main
-        className="relative flex flex-col items-center justify-center h-screen w-full z-0 before:absolute before:inset-0 before:bg-myCharacter before:bg-cover before:bg-center before:z-0">
+        className="relative flex flex-col items-center justify-center min-h-screen w-full z-0 before:absolute before:inset-0 before:bg-myCharacter before:bg-cover before:bg-center before:z-0">
+        <div className="absolute inset-0 w-full h-full bg-black opacity-70" />
         <div
-          className="flex flex-col justify-center items-center bg-black bg-opacity-70 text-white w-5/6 my-20 z-10 gap-10 p-16">
+          className="flex flex-col justify-between py-10 text-white min-h-[calc(100vh-100px)] min-w-[90%] z-10 mt-24">
 
           <section className="w-full flex justify-center items-center">
-            <h1 className="text-3xl text-white tracking-widest">CHARACTER NAME</h1>
+            <h1 className="text-4xl text-white tracking-widest">CHARACTER NAME</h1>
           </section>
 
-          <section className="w-full flex laptop:flex-row justify-between">
+          <section className="w-full flex justify-between">
 
             <article className="w-full xl:w-1/2 flex flex-col items-center">
-              <Button text="Race" className="mb-8" />
+              <h2 className="text-3xl mb-8">Race:</h2>
               <div className="flex items-center justify-between gap-4">
                 <button className="text-4xl text-white hover:text-gray-400" onClick={handlePrevPageRace}>
                   <FaChevronLeft />
                 </button>
                 {currentRace && (
-                  <article className="flex flex-col justify-center items-center gap-4 min-w-44">
+                  <article className="flex flex-col justify-center items-center gap-4 min-w-52">
                     <h2 className=" text-2xl flex">{currentRace.name}</h2>
-                    <img src={currentRaceImage} alt="Character" className="max-h-44 shadow-none" />
+                    <img src={currentRaceImage} alt="Character" className="max-h-52 shadow-none" />
                   </article>
                 )}
                 <button className="text-4xl text-white hover:text-gray-400" onClick={handleNextPageRace}>
@@ -189,7 +193,7 @@ const MyCharacterPage: React.FC<MyCharacterProps> = ({ raceChange, classChange }
             </article>
 
             <article className="w-full xl:w-1/2 flex flex-col items-center">
-              <Button text="Class" className="mb-8" />
+              <h2 className="text-3xl mb-8">Class:</h2>
               <div className="flex items-center gap-4">
                 <button className="text-4xl text-white hover:text-gray-400" onClick={handlePrevPageClass}>
                   <FaChevronLeft />
@@ -198,7 +202,7 @@ const MyCharacterPage: React.FC<MyCharacterProps> = ({ raceChange, classChange }
                   <article className="flex flex-col items-center gap-4">
                     <h2 className="flex text-2xl">{currentClass.name}</h2>
                     <div
-                      className="flex justify-center items-center w-44 h-44 overflow-hidden">
+                      className="flex justify-center items-center w-52 h-52 overflow-hidden">
                       <img
                         src={currentClassImage}
                         alt="Character"
@@ -216,13 +220,13 @@ const MyCharacterPage: React.FC<MyCharacterProps> = ({ raceChange, classChange }
 
           <section className="w-full flex xl:flex-row justify-between">
             <article className="flex flex-col items-center w-full xl:w-1/2">
-              <Button text="Ability Classes" className="mb-8" />
+              <h2 className="text-3xl mb-8">Ability Scores:</h2>
               <div className="grid grid-cols-1 xl:grid-cols-2 gap-y-6 gap-x-40">
-                {[...Array(6)].map((_, index) => (
+                {abilityData.map((ability, index) => (
                   <div key={index} className="flex items-center">
-                    <label className="text-lg w-32">Ability {index + 1}:</label>
+                    <label className="text-lg w-32">{ability.full_name}:</label>
                     <Counter
-                      value={abilities[index]}
+                      value={abilityValue[index]}
                       onChange={(newValue) => handleAbilityChange(index, newValue)}
                     />
                   </div>
@@ -231,8 +235,8 @@ const MyCharacterPage: React.FC<MyCharacterProps> = ({ raceChange, classChange }
             </article>
 
             <article className="flex flex-col items-center w-full xl:w-1/2">
-              <Button text="Equipments" className="mb-8" />
-              <div className="grid grid-cols-1 xl:grid-cols-2 gap-y-4 gap-x-40">
+              <h2 className="text-3xl mb-8">Equipments:</h2>
+              <div className="grid grid-cols-1 xl:grid-cols-2 gap-y-10 gap-x-40">
                 {['Item 1', 'Item 2', 'Item 3', 'Item 4', 'Item 5', 'Item 6', 'Item 7', 'Item 8', 'Item 9', 'Item 10'].map((item, index) => (
                   <li key={index} className="list-disc list-inside text-lg">
                     {item}
