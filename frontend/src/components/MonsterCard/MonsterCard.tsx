9830e8a28a90a323114a341148763951489ef51c
Mats <mats.kvanvik@hotmail.com>
2024-10-11 13:48:28 +0200

    feat: add functions to monstercard to open modal

--- frontend/src/components/MonsterCard/MonsterCard.tsx
+++ frontend/src/components/MonsterCard/MonsterCard.tsx
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import useMonster from '../../hooks/useMonsters.ts';
+import useMonsters from '../../hooks/useMonsters.ts';
 import NoMonsterImageFound from '../../assets/images/no_monster_image_found.jpg';
 import MonsterCardInfo from './MonsterReviewModal.tsx';
 
 interface MonsterCardProps {
   monsterName: string;
   onLoad: () => void;
-  onClick: () => void;
+  onClickFunc: () => void;
 }
 
-const MonsterCard: React.FC<MonsterCardProps> = ({ monsterName, onLoad, onClick }) => {
-  const monsterInfo = useMonster(monsterName);
+const MonsterCard: React.FC<MonsterCardProps> = ({ monsterName, onLoad, onClickFunc }) => {
+  const monsterInfo = useMonsters(monsterName);
   const [imageLoaded, setImageLoaded] = useState(false);
 
   useEffect(() => {
@@ -18,7 +18,7 @@ const MonsterCard: React.FC<MonsterCardProps> = ({ monsterName, onLoad, onClick 
       setImageLoaded(true);
       onLoad();
     }
-  }, [monsterInfo.img, onLoad]);
+  }, [monsterInfo.img, onLoad, monsterInfo.name]);
 
   const handleImageLoad = () => {
     setImageLoaded(true);
@@ -38,13 +38,12 @@ const MonsterCard: React.FC<MonsterCardProps> = ({ monsterName, onLoad, onClick 
   const monsterImageURL = monsterInfo.img ? `https://www.dnd5eapi.co${monsterInfo.img}` : NoMonsterImageFound;
 
   return (
-    <div
-      className="flex flex-col items-center justify-center bg-black max-w-[20vw] rounded-[10px] shadow-[rgba(0,0,0,0.15)_1.95px_1.95px_2.6px]">
+    <div className="flex flex-col items-center justify-center bg-black max-w-[20vw] rounded-[10px] shadow-[rgba(0,0,0,0.15)_1.95px_1.95px_2.6px]">
       <MonsterCardInfo name={monsterName} />
       <img
         src={monsterImageURL}
         alt={monsterInfo.img ? 'Image of the monster' : 'No monster image found'}
-        className="max-w-[15vw] rounded-[15px] shadow-[0_2px_2px_0_rgba(0,0,0,1) top-0 pt-0 mt-0]"
+        className="max-w-[15vw] rounded-[15px] shadow-[0_2px_2px_0_rgba(0,0,0,1) top-0 pt-0 mt-0"
         onLoad={handleImageLoad}
         onError={handleImageError}
         style={{ display: imageLoaded ? 'block' : 'none' }}
@@ -54,7 +53,7 @@ const MonsterCard: React.FC<MonsterCardProps> = ({ monsterName, onLoad, onClick 
       <div className="mb-[3px] p-[5px] text-white font-semibold">
         <p>Type: {monsterInfo.type}</p>
         <p>HP: {monsterInfo.hp}</p>
-        <button onClick={onClick}>Get more info! </button>
+        <button onClick={onClickFunc}>Get more info!</button>
       </div>
     </div>
   );
