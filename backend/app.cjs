92fa095fab997f65c7dd1bd207d425ef3478aae1
August Solli Middelkoop <august.kampen@gmail.com>
2024-10-18 11:24:19 +0200

    Feature: Backend is now connected to mongoDB compass
    Temporarily using js, but will change to typescript. DB is now connected to backend.

--- backend/app.cjs
+++ backend/app.cjs
@@ -2,21 +2,32 @@ const express = require('express');
 const {connectToDb, getDb} = require('./db.cjs');
 const app = express();
 
-app.get("/MyCharacter", (req, res) => {
-  let players = []
-  db.collection("MyCharacter").find().sort({username:1}).forEach((player)=>
-    players.push(player)
-  ).then(()=>{
-    return res.status(200).json(players)
-  }).catch(err=>{
-    res.status(500).json({error:err})
-  })
-})
+
 
 let db
 connectToDb((err)=>{
   if (!err){
-    app.listen(4000, ()=> console.log(`Server listening on port 4000`));
+    app.listen(4000, ()=> {
+      console.log(`Server listening on port 4000`)
+    })
+    db=getDb()
+    console.log(db)
   }
-  db=getDb()
+
+})
+
+app.get('/MyCharachter', (req, res)=>{
+  let players = []
+
+  db.collection('MyCharachter')
+    .find()
+    .sort({username:1})
+    .forEach(player=>players.push(player))
+    .then(()=>{
+      res.status(200).json(players)
+      console.log(db)
+    })
+    .catch(()=>{
+      res.status(500).json({error: 'Something went wrong'})
+    })
 })
--- backend/db.cjs
+++ backend/db.cjs
@@ -4,9 +4,10 @@ let dbConnection
 
 module.exports = {
   connectToDb: (cb) =>{
-    MongoClient.connect('mongodb://admin:adminpassordetditt@it2810-20.idi.ntnu.no:27017/?directConnection=true&authSource=admin&appName=mongosh+2.3.2/Profile')
+    MongoClient.connect('mongodb://admin:adminpassordetditt@it2810-20.idi.ntnu.no:27017/Profile?directConnection=true&authSource=admin&appName=mongosh+2.3.2')
       .then((client)=>{
         dbConnection = client.db()
+        console.log(client.db)
         return cb()
       })
 
