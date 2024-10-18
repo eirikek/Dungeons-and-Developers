9b63c4570eeb838ab0e5fa431d22cb473261e8ae
August Solli Middelkoop <august.kampen@gmail.com>
2024-10-18 13:06:01 +0200

    Feature: post method for player/user
    Temporarily using js, but will change to typescript. Possible to use post new player.

--- backend/app.cjs
+++ backend/app.cjs
@@ -1,7 +1,8 @@
 const express = require('express');
 const {connectToDb, getDb} = require('./db.cjs');
+const { ObjectId } = require('mongodb');
 const app = express();
-
+app.use(express.json());
 
 
 let db
@@ -30,4 +31,34 @@ app.get('/MyCharachter', (req, res)=>{
     .catch(()=>{
       res.status(500).json({error: 'Something went wrong'})
     })
+})
+
+app.get('/MyCharachter/:username', (req, res)=>{
+  db.collection('MyCharachter')
+    .findOne({username:req.params.username})
+    .then(usernames => {
+      res.status(200).json(usernames)
+    })
+    .catch(err => res.status(500).json({error: 'Something went wrong'}))
+})
+
+//I don't understand why it won't work...
+app.get('/MyCharachter/:userID', (req, res)=>{
+  db.collection('MyCharachter')
+    .findOne({userID: req.params.userID})
+    .then(ids => {
+      res.status(200).json(ids)
+    })
+    .catch(err => res.status(500).json({error: 'Something went wrong'}))
+})
+
+app.post('/MyCharachter', (req, res)=>{
+  const player = req.body
+  db.collection('MyCharachter')
+    .insertOne(player)
+    .then(result => {
+      res.status(201).json(result)
+    })
+    .catch(err => {res.status(500).json({error: 'Something went wrong'})})
+
 })
