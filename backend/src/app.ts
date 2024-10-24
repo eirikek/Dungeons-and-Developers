0890a8a4f9d0981b97090498bd955feee86f577d
eirikek <eirik.engen.kvam@gmail.com>
2024-10-24 19:04:25 +0200

    backend: store all monsters in database

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,27 +1,25 @@
- import express from 'express';
-  import { ApolloServer } from 'apollo-server';
-  import mongoose from "mongoose";
+import express from 'express';
+import { ApolloServer } from 'apollo-server';
+import mongoose from 'mongoose';
 
-  import typeDefs from './graphql/typeDefs.js';
-  import resolvers from './graphql/resolvers.js';
-  const app = express();
-  app.use(express.json());
-  const MONGODBURLPROFILE ='mongodb://admin:adminpassordetditt@it2810-20.idi.ntnu.no:27017/Profile?directConnection=true&authSource=admin&appName=mongosh+2.3.2'
+import typeDefs from './graphql/typeDefs.js';
+import resolvers from './graphql/resolvers.js';
 
-
-
+const app = express();
+app.use(express.json());
+const MONGODBURLPROFILE = 'mongodb://admin:adminpassordetditt@it2810-20.idi.ntnu.no:27017/Profile?directConnection=true&authSource=admin&appName=mongosh+2.3.2';
 
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
-  })
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
 
-  mongoose.connect(MONGODBURLPROFILE)
-  .then(()=>{
-    console.log(("mongoDB connection connected"))
-    return server.listen({port: 5000})
-    }
-  ).then((result)=>{
-    console.log(result.url)
-  })
+mongoose.connect(MONGODBURLPROFILE)
+  .then(() => {
+      console.log(('mongoDB connection connected'));
+      return server.listen({ port: 4000 });
+    },
+  ).then((result) => {
+  console.log(result.url);
+});
--- backend/src/graphql/typeDefs.ts
+++ backend/src/graphql/typeDefs.ts
@@ -1,92 +1,91 @@
 import { gql } from 'apollo-server';
 
-export default  gql`
-  type Player{
-    username: String!
-    userID: Int!
-    characterName: String!
-    characterClass: String!
-    race: String!
-    abilityScores: [Int!]!
-   }
-   
-   input RegisterInput{
-      username: String!
-   }
-   
-   input LoginInput{
-      username: String!
-      }
-   type Query {
-      user(id: ID!): User    
-      }
-   type Mutation {
-      registerUser(registerInput: RegisterInput): User
-      loginUser(loginInput: LoginInput): User
-      }
-    
-   input PlayerInput {
-     username: String!
-     userID: Int!
-     characterName: String!
-     characterClass: String!
-     race: String!
-     abilityScores: [Int!]!
-   }
-    
-   input EditPlayerInput {
-     username: String!
-     userID: Int!
-     characterName: String!
-     characterClass: String!
-     race: String!
-     abilityScores: [Int!]!
-   }
-   
-   type Query {
-     player(ID: ID!): Player!
-     getPlayer(amount: Int): [Player]
-   }
-    
-   type Mutation {
-    createPlayer(playerInput: PlayerInput): Player!
-    deletePlayer(ID: ID!): Boolean
-    editPlayer(ID: ID!, editRecipeInput: EditPlayerInput): Boolean
-  }
-    
-    
-  type Monster {
-    name: String!
-    size: String!
-    type: String!
-    alignment: String!
-    hit_points: Int!
-    hit_dice: String!
-    strength: Int!
-    dexterity: Int!
-    intelligence: Int!
-    wisdom: Int!
-    charisma: Int!
-    image: String!
-  }
-  type Ability {
-    name: String!
-    score: Int!
-  }
-  type Character {
-    name: String!
-    class: String!
-    race: String!
-    abilityScores: [Ability!]!
-  }
-  type User {
-    userName: String!
-    userID: ID!
-    character: Character!
-  }
+export default gql`
+    type Player {
+        username: String!
+        userID: Int!
+        characterName: String!
+        characterClass: String!
+        race: String!
+        abilityScores: [Int!]!
+    }
 
-  type Query {
-    user(id: ID!): User!
-    monster(id: String!): Monster!
-  }
+    input RegisterInput {
+        username: String!
+    }
+
+    input LoginInput {
+        username: String!
+    }
+
+    input PlayerInput {
+        username: String!
+        userID: Int!
+        characterName: String!
+        characterClass: String!
+        race: String!
+        abilityScores: [Int!]!
+    }
+
+    input EditPlayerInput {
+        username: String!
+        userID: Int!
+        characterName: String!
+        characterClass: String!
+        race: String!
+        abilityScores: [Int!]!
+    }
+
+    type Monster {
+        index: String!
+        name: String!
+        size: String!
+        type: String!
+        alignment: String!
+        hit_points: Int!
+        hit_dice: String!
+        strength: Int!
+        dexterity: Int!
+        intelligence: Int!
+        wisdom: Int!
+        charisma: Int!
+        image: String
+    }
+
+    type Ability {
+        name: String!
+        score: Int!
+    }
+
+    type Character {
+        name: String!
+        class: String!
+        race: String!
+        abilityScores: [Ability!]!
+    }
+
+    type User {
+        userName: String!
+        userID: ID!
+        character: Character!
+    }
+
+    type Query {
+        player(ID: ID!): Player!
+        getPlayer(amount: Int): [Player]
+        monsters: [Monster!]!
+        monster(id: String!): Monster!
+        user(id: ID!): User!
+    }
+
+    type Mutation {
+        createPlayer(playerInput: PlayerInput): Player!
+        deletePlayer(ID: ID!): Boolean
+        editPlayer(ID: ID!, editRecipeInput: EditPlayerInput): Boolean
+
+        registerUser(registerInput: RegisterInput): User
+        loginUser(loginInput: LoginInput): User
+
+        fetchMonsters: String!
+    }
 `;
