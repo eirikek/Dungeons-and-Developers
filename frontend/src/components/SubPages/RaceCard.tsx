8a2ab109990e22763b83fe9dac9bc86d3ddf3395
August Solli Middelkoop <august.kampen@gmail.com>
2024-10-25 10:46:59 +0200

    Enhancement: Styling for races
    Improved styling for more Race attributes.

--- frontend/src/components/SubPages/RaceCard.tsx
+++ frontend/src/components/SubPages/RaceCard.tsx
@@ -17,7 +17,7 @@ interface RaceCardProps {
 const RaceCard: React.FC<RaceCardProps> = ({ name, description, imageUrl, alignment, size, size_description, speed }) => {
   return (
     <motion.section
-      className="flex flex-col xl:flex-row xl:h-60 w-full justify-between items-center p-8 xl:p-12 rounded-lg bg-black bg-opacity-80 gap-10"
+      className="flex flex-col xl:flex-row xl:h-90 w-full justify-between items-center p-8 xl:p-12 rounded-lg bg-black bg-opacity-80 gap-10"
       initial="hidden"
       whileInView="visible"
       viewport={{ once: true, amount: 0.1 }}
@@ -27,7 +27,7 @@ const RaceCard: React.FC<RaceCardProps> = ({ name, description, imageUrl, alignm
         visible: { opacity: 1, y: 0 },
       }}
     >
-      <div className="flex flex-col xl:flex-row gap-5 justify-center items-center">
+      <div className="flex flex-col xl:flex-row gap-5 justify-center items-center ">
         <img src={imageUrl} alt={name} className="max-w-36 xl:max-w-28 shadow-none" />
         <div>
           <h2 className="sub-header bold">{name}</h2>
