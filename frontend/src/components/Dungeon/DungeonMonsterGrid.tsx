5a0491dd9ce7f1c1b0339fecfee033f78d1fd6c9
eirikek <eirik.engen.kvam@gmail.com>
2024-10-28 16:08:28 +0100

    feature: show total dungeon hp on dungeon page

--- frontend/src/components/Dungeon/DungeonMonsterGrid.tsx
+++ frontend/src/components/Dungeon/DungeonMonsterGrid.tsx
@@ -4,25 +4,33 @@ import MonsterCard from '../MonsterCard/MonsterCard';
 
 const DungeonMonsterGrid = () => {
   const { dungeonMonsters } = useContext(DungeonContext);
+  const totalHp = dungeonMonsters.reduce((sum, monster) => sum + monster.hp, 0);
 
   return (
-    <div
-      className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-10 xl:gap-x-28 gap-y-5 place-items-center transition-opacity duration-500 p-5">
-      {dungeonMonsters.length === 0 ? (
-        <div
-          className="absolute inset-0 flex items-center justify-center h-1/2 sub-header">
-          <p>No monsters in dungeon</p>
-        </div>
-      ) : (
-        dungeonMonsters.map((monster, idx) => (
-          <MonsterCard
-            key={idx}
-            {...monster}
-            hp={monster.hp}
-          />
-        ))
+    <>
+      {dungeonMonsters.length > 0 && (
+        <h2 className="mt-5 sub-header">
+          Total Dungeon HP: {totalHp}
+        </h2>
       )}
-    </div>
+      <div
+        className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-x-10 xl:gap-x-28 gap-y-5 place-items-center transition-opacity duration-500 p-5">
+        {dungeonMonsters.length === 0 ? (
+          <div
+            className="absolute inset-0 flex items-center justify-center h-1/2 sub-header">
+            <p>No monsters in dungeon</p>
+          </div>
+        ) : (
+          dungeonMonsters.map((monster, idx) => (
+            <MonsterCard
+              key={idx}
+              {...monster}
+              hp={monster.hp}
+            />
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
