f470d9d9aa8c56575874c0591388fdc1365ebd3a
Mats <mats.kvanvik@hotmail.com>
2024-11-13 14:12:53 +0100

    feature: position and resize of arrow

--- frontend/src/components/ArrowSVG/DownArrow.tsx
+++ frontend/src/components/ArrowSVG/DownArrow.tsx
@@ -2,15 +2,15 @@ export default function DownArrow() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
+      width="90"
+      height="40"
       viewBox="0 0 24 24"
       fill="none"
       stroke="currentColor"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
-      className={`w-[5000px] text-white `}
+      className={`self-center mt-[50vh]  text-white animate-bounce z-10`}
     >
       <path d="m6 9 6 6 6-6" />
     </svg>
