2b3754396d7e056c5db0df0d59eca367185f538d
Mats <mats.kvanvik@hotmail.com>
2024-11-13 18:38:44 +0100

    feature: add button back to homepage from notfound

--- frontend/src/pages/mainPages/notFoundPage.tsx
+++ frontend/src/pages/mainPages/notFoundPage.tsx
@@ -1,7 +1,15 @@
+import CustomButton from '../../components/CustomButton/CustomButton.tsx';
+
 export default function NotFoundPage() {
   return (
-    <div>
-      <h1>404 Page</h1>
+    <div className="flex flex-col items-center justify-center w-screen h-screen relative">
+      <div className="flex flex-col items-center justify-center flex-grow">
+        <h1 className="text-4xl">404</h1>
+        <h4>Page Not Found</h4>
+      </div>
+      <div className="absolute bottom-6">
+        <CustomButton className="text-xl" text="Back to homepage" linkTo="/home" />
+      </div>
     </div>
   );
 }
