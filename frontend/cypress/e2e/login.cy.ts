17f9693a3aa44d93049dedbbb88ed49e44901fe2
Mats <mats.kvanvik@hotmail.com>
2024-11-18 15:32:44 +0100

    feature: test adds monster to dungeon

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -24,6 +24,23 @@ describe('Logs user inn, views monsters, adds to dungeon', () => {
     cy.get('div').children('div').children('div').get('h2').should('exist').should('have.text', 'Aboleth');
 
     // Add monster to dungeon
-    cy.get('div').children('div').children('div').get('button').eq(6).should('have.text', 'Add to dungeon').click();
+    // Check if the button text is "Add to dungeon" and click it if true
+    cy.get('div')
+      .children('div')
+      .children('div')
+      .get('button')
+      .eq(6)
+      .then((button) => {
+        // conditional click, if monster already added to dungeon or not
+        if (button.text() === 'Add to dungeon') {
+          cy.wrap(button).click();
+        } else {
+          cy.wrap(button).click();
+          cy.wait(200);
+          cy.wrap(button).click();
+        }
+      });
+
+    // TODO: Check monster successfully added to page, apply filtering to monstersearch
   });
 });
