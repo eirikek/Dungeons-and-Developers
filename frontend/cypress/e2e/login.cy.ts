b9d2cc5340bc34d2903720c65233de6bf3ba05a1
Mats <mats.kvanvik@hotmail.com>
2024-11-18 15:20:57 +0100

    feature: test now goes to monster, adds to dungeon

--- frontend/cypress/e2e/login.cy.ts
+++ frontend/cypress/e2e/login.cy.ts
@@ -1,16 +1,29 @@
-describe('template spec', () => {
-  it('passes', () => {
+describe('Logs user inn, views monsters, adds to dungeon', () => {
+  beforeEach(() => {
+    // Set viewport to desktop size
+    cy.viewport(1280, 720);
+  });
+
+  it('logs in', () => {
     /*
       As a user, I wish to login to the app, view monsters. And add a monster to my dungeon
      */
 
-    // login
+    // Log in
     cy.visit('http://localhost:5173/');
     cy.get('input').type('Cypress test user');
     cy.get('button').first().should('exist').should('have.text', 'Log in').click();
 
-    // Navigate to monster page
-    cy.get('button').first().click();
-    cy.get('.sub-header').first().should('have.text', 'Monsters').get('button').first().should('exist').click();
+    // Navigates to monster page
+    cy.get('a').eq(2).should('have.text', 'Monsters').click();
+
+    // Search for monster
+    cy.get('input').type('Aboleth');
+
+    // Make sure monster exists
+    cy.get('div').children('div').children('div').get('h2').should('exist').should('have.text', 'Aboleth');
+
+    // Add monster to dungeon
+    cy.get('div').children('div').children('div').get('button').eq(6).should('have.text', 'Add to dungeon').click();
   });
 });
