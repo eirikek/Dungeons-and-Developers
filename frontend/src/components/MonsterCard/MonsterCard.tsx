1c4ae3b6d7e395a0b54b55a5dfb4391700c6e142
August Solli Middelkoop <august.kampen@gmail.com>
2024-12-03 11:22:02 +0100

    Enhancement/Fix: Fixed traversable by keys issues.
    MonsterCard focus attributes for styling

--- frontend/src/components/MonsterCard/MonsterCard.tsx
+++ frontend/src/components/MonsterCard/MonsterCard.tsx
@@ -63,7 +63,7 @@ const MonsterCard = ({ id, name, type, hit_points, alignment, size, image }: Mon
       <div
         onClick={handleCardClick}
         className="flex flex-col items-center justify-between bg-black pb-5 w-[75vw] md:w-[42vw] xl:w-[22vw] 2xl:w-[18vw] h-[40vh] sm:h-[45vh] md:h-[35vh] rounded-lg overflow-hidden
-           transition-transform duration-300 ease-in-out transform focus:hover:shadow-lg focus:hover:shadow-black focus:hover:scale-105 hover:scale-105 hover:shadow-lg hover:shadow-black cursor-pointer card monster-card"
+           transition-transform duration-300 ease-in-out transform focus:hover:shadow-lg focus:shadow-black focus:scale-105 hover:scale-105 hover:shadow-lg hover:shadow-black cursor-pointer card monster-card"
         aria-label={name}
         data-testid={`${name}-monster-card`}
         onKeyDown={handleKeyDown}
