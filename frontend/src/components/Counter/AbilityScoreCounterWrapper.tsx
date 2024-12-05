829ab04c9c762ac2cb37b51b6aa83599eba88bef
August Solli Middelkoop <august.kampen@gmail.com>
2024-12-05 15:22:59 +0100

    Docs: Written docs for AbilityScoreCounterWrapper.tsx

--- frontend/src/components/Counter/AbilityScoreCounterWrapper.tsx
+++ frontend/src/components/Counter/AbilityScoreCounterWrapper.tsx
@@ -1,5 +1,12 @@
 import Counter from './Counter.tsx';
-
+/**
+ * A custom wrapper for wrapping a custom Counter component.
+ * It manages and the change in value and updates the parent when the value is finalized.
+ *
+ * @param {string} abilityName - Name of the ability score to be updated.
+ * @param {number} initialValue - The initial value to be displayed in the counter.
+ * @param {(value: number) => void} onUpdate - Function handle updated value.
+ */
 const AbilityCounterWrapper = ({
   initialValue,
   onUpdate,
