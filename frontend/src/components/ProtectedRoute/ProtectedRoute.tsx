67937ca28ec6c09410e4a0d9fa41c75083b024fc
August Solli Middelkoop <august.kampen@gmail.com>
2024-12-05 17:12:37 +0100

    Docs: Written docs for ProtectedRoute.tsx

--- frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,10 +1,17 @@
-import { ReactNode } from 'react';
 import { Navigate } from 'react-router-dom';
+import ProtectedRouteProps from '../../interfaces/ProtectedRouteProps.ts';
 
-interface ProtectedRouteProps {
-  children: ReactNode;
-}
-
+/**
+ * ProtectedRoute component is used to wrap a path that requires the user to be authenticated.
+ * If the user isn't authenticated (i.e., no token is found in localStorage), they are redirected to the homepage.
+ * If the user is authenticated (i.e., a token exists), the children components are rendered.
+ *
+ * @param {React.ReactNode} children - If the user is authenticated the children will be rendered in the component
+ *
+ * Features:
+ * - Looks for a token in `localStorage` to see if the user is authenticated.
+ * - Redirects the user to the homepage if the token isn't found.
+ */
 function ProtectedRoute({ children }: ProtectedRouteProps) {
   const token = localStorage.getItem('token');
   return token ? <>{children}</> : <Navigate to="/" />;
