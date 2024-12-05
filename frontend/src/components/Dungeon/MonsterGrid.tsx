b50886855ce7c6e8a4fe185efa993932a12b6f47
August Solli Middelkoop <august.kampen@gmail.com>
2024-12-05 15:44:09 +0100

    Docs: Written docs for MonsterGrid.tsx

--- frontend/src/components/Dungeon/MonsterGrid.tsx
+++ frontend/src/components/Dungeon/MonsterGrid.tsx
@@ -1,13 +1,19 @@
 import { useReactiveVar } from '@apollo/client';
 import { useMemo } from 'react';
-import type { MonsterCardProps } from '../../interfaces/MonsterCardProps';
 import { dungeonMonstersVar } from '../../utils/apolloVars.ts';
 import MonsterCard from '../MonsterCard/MonsterCard';
+import MonsterGridProps from '../../interfaces/MonsterGridProps.ts';
 
-interface MonsterGridProps {
-  monsters?: MonsterCardProps[];
-  isDungeonPage?: boolean;
-}
+/**
+ * Renders a MonsterGrid component marking whether
+ * they are in the dungeon based on the context.
+ *
+ * It uses `useReactiveVar` from Apollo Client.
+ *
+ * @param {Array} monsters - The list of monsters to display.
+ * @param {boolean} isDungeonPage - Boolean value to indicate if it is on the dungeon page or not.
+ *
+ */
 
 const MonsterGrid = ({ monsters = [], isDungeonPage = false }: MonsterGridProps) => {
   const dungeonMonsters = useReactiveVar(dungeonMonstersVar);
